72cdaa83d8016088a7c715bb65f98d5221b65879
Hafiz Zia <zia@deltalabs.io>
2019-03-01 20:06:14 +0500

    Friday's commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -10,32 +13,34 @@ class ArticlesController < ApplicationController
 
 
     def create
+        #debugger
         @article = Article.new(article_params)
+        @article.user = current_user
         if @article.save
             flash[:notice] = "Successfully created"
             redirect_to article_path(@article)
         else
-            render 'new'        
-        end        
+            render 'new'
+        end
     end
 
     def show
-        
+
     end
 
     def destroy
-        
+
         @article.destroy
         flash[:notice] = "Successfully deleted"
         redirect_to articles_path
     end
 
     def edit
-        
+
     end
 
     def update
-        
+
         if @article.update(article_params)
             flash[:notice]="Successfully updated"
             redirect_to article_path(@article)
@@ -47,13 +52,18 @@ class ArticlesController < ApplicationController
     private
 
     def set_article
-
         @article = Article.find(params[:id])
-        
     end
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+        if current_user != @article.user and !current_user.admin?
+            flash[:danger] = "You can only edit or delete your own articles"
+            redirect_to root_path
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
+  get 'signup',to: 'users#new'
+  resources :users, except: [:new]
   resources :articles
+
+  get 'login',to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
